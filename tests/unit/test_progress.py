54d5fffe1654fcaac7372791e5494bd1fb566cbf
Jonathan Woo <jonathan.woo@mail.utoronto.ca>
2025-08-07 15:05:40 -0400

    test: enhance progress tests with dummy data fixture and reentrant testing

--- tests/unit/test_progress.py
+++ tests/unit/test_progress.py
@@ -1,5 +1,19 @@
 import json
 from progresslib import ProgressController, ProgressState
+import pytest
+
+
+@pytest.fixture
+def dummy_progress_data():
+    return "test_video_key", ProgressController.ProgressItem(
+        original_video_name="Test Video",
+        new_video_name="PROCESSED Test Video",
+        original_playlist_name="Test Playlist",
+        new_playlist_name="PROCESSED Test Playlist",
+        original_video_id="123",
+        original_playlist_id="456",
+        new_playlist_id="789",
+    )
 
 
 def test_reset_progress(progress_controller):
@@ -16,122 +30,97 @@ def test_load_progress_empty(progress_controller):
     assert progress_data == progress_controller.default_progress
 
 
-def test_add_item(progress_controller):
+def test_add_item(progress_controller, dummy_progress_data):
     # Test adding a new item to the progress
-    progress_controller.add_item(
-        ProgressState.DOWNLOADING,
-        "video1",
-        ProgressController.ProgressItem(
-            original_video_name="Test Video 1",
-            new_video_name="PROCESSED Test Video 1",
-            original_playlist_name="Test Playlist",
-            new_playlist_name="PROCESSED Test Playlist",
-            original_video_id="123",
-            original_playlist_id="456",
-            new_playlist_id="789",
-        ),
-    )
+    dummy_key, dummy_item = dummy_progress_data
+    progress_controller.add_item(ProgressState.DOWNLOADING, dummy_key, dummy_item)
 
     progress_data = progress_controller.load_progress()
-    assert "video1" in progress_data[ProgressState.DOWNLOADING]
-    item = progress_data[ProgressState.DOWNLOADING]["video1"]
-    assert item.original_video_name == "Test Video 1"
-    assert item.new_video_name == "PROCESSED Test Video 1"
-    assert item.original_playlist_name == "Test Playlist"
-    assert item.new_playlist_name == "PROCESSED Test Playlist"
-    assert item.original_video_id == "123"
-    assert item.original_playlist_id == "456"
-    assert item.new_playlist_id == "789"
+    assert progress_data[ProgressState.DOWNLOADING][dummy_key] == dummy_item
 
 
-def test_load_progress_with_data(progress_controller):
+def test_load_progress_with_data(progress_controller, dummy_progress_data):
     # Test loading progress when data exists
-    to_add = ProgressController.ProgressItem(
-        original_video_name="Test Video 1",
-        new_video_name="PROCESSED Test Video 1",
-        original_playlist_name="Test Playlist",
-        new_playlist_name="PROCESSED Test Playlist",
-        original_video_id="123",
-        original_playlist_id="456",
-        new_playlist_id="789",
-    )
-    progress_controller.add_item(ProgressState.DOWNLOADING, "video1", to_add)
+    dummy_key, dummy_item = dummy_progress_data
+    progress_controller.add_item(ProgressState.DOWNLOADING, dummy_key, dummy_item)
 
     progress_data = progress_controller.load_progress()
-    assert progress_data[ProgressState.DOWNLOADING]["video1"] == to_add
+    assert progress_data[ProgressState.DOWNLOADING][dummy_key] == dummy_item
 
 
-def test_move_item(progress_controller):
+def test_move_item(progress_controller, dummy_progress_data):
     # Test moving an item from one state to another
-    progress_controller.add_item(
-        ProgressState.DOWNLOADING,
-        "video1",
-        ProgressController.ProgressItem(
-            original_video_name="Test Video 1",
-            new_video_name="PROCESSED Test Video 1",
-            original_playlist_name="Test Playlist",
-            new_playlist_name="PROCESSED Test Playlist",
-            original_video_id="123",
-            original_playlist_id="456",
-            new_playlist_id="789",
-        ),
-    )
+    dummy_key, dummy_item = dummy_progress_data
+    progress_controller.add_item(ProgressState.DOWNLOADING, dummy_key, dummy_item)
 
     progress_controller.move_item(
-        ProgressState.DOWNLOADING, ProgressState.DOWNLOADED, "video1"
+        ProgressState.DOWNLOADING, ProgressState.DOWNLOADED, dummy_key
     )
 
     progress_data = progress_controller.load_progress()
-    assert "video1" in progress_data[ProgressState.DOWNLOADED]
-    item = progress_data[ProgressState.DOWNLOADED]["video1"]
-    assert item.original_video_name == "Test Video 1"
-    assert item.new_video_name == "PROCESSED Test Video 1"
+    assert progress_data[ProgressState.DOWNLOADED][dummy_key] == dummy_item
 
 
-def test_read_advance(progress_controller):
+def test_read_advance(progress_controller, dummy_progress_data):
     # Test reading and advancing an item from one state to another
-    to_add = ProgressController.ProgressItem(
-        original_video_name="Test Video 1",
-        new_video_name="PROCESSED Test Video 1",
-        original_playlist_name="Test Playlist",
-        new_playlist_name="PROCESSED Test Playlist",
-        original_video_id="123",
-        original_playlist_id="456",
-        new_playlist_id="789",
-    )
-    progress_controller.add_item(ProgressState.DOWNLOADING, "video1", to_add)
+    dummy_key, dummy_item = dummy_progress_data
+    progress_controller.add_item(ProgressState.DOWNLOADING, dummy_key, dummy_item)
 
     key, item = progress_controller.read_and_move_next_item(
         ProgressState.DOWNLOADING, ProgressState.DOWNLOADED
     )
 
-    assert key == "video1"
-    assert item == to_add
+    progress_data = progress_controller.load_progress()
+    assert progress_data[ProgressState.DOWNLOADED][key] == dummy_item
+    assert item == dummy_item
+    assert progress_data[ProgressState.DOWNLOADING] == {}
 
     key, item = progress_controller.read_and_move_next_item(
         ProgressState.DOWNLOADED, ProgressState.PROCESSING
     )
 
-    assert key == "video1"
-    assert item == to_add
+    progress_data = progress_controller.load_progress()
+    assert progress_data[ProgressState.PROCESSING][key] == dummy_item
+    assert item == dummy_item
+    assert progress_data[ProgressState.DOWNLOADED] == {}
 
     key, item = progress_controller.read_and_move_next_item(
         ProgressState.PROCESSING, ProgressState.PROCESSED
     )
 
-    assert key == "video1"
-    assert item == to_add
+    progress_data = progress_controller.load_progress()
+    assert progress_data[ProgressState.PROCESSED][key] == dummy_item
+    assert item == dummy_item
+    assert progress_data[ProgressState.PROCESSING] == {}
 
     key, item = progress_controller.read_and_move_next_item(
         ProgressState.PROCESSED, ProgressState.UPLOADING
     )
 
-    assert key == "video1"
-    assert item == to_add
+    progress_data = progress_controller.load_progress()
+    assert progress_data[ProgressState.UPLOADING][key] == dummy_item
+    assert item == dummy_item
+    assert progress_data[ProgressState.PROCESSED] == {}
 
     key, item = progress_controller.read_and_move_next_item(
         ProgressState.UPLOADING, ProgressState.UPLOADED
     )
 
-    assert key == "video1"
-    assert item == to_add
+    progress_data = progress_controller.load_progress()
+    progress_data = progress_controller.load_progress()
+    assert progress_data[ProgressState.UPLOADED][key] == dummy_item
+    assert item == dummy_item
+    assert progress_data[ProgressState.UPLOADING] == {}
+
+
+def test_reentrant(progress_controller, dummy_progress_data):
+    # Test that the lock is reentrant
+    dummy_key, dummy_item = dummy_progress_data
+
+    progress_controller.lock_file()
+    progress_controller.add_item(ProgressState.DOWNLOADING, dummy_key, dummy_item)
+    progress_controller.unlock_file()
+
+    progress_data = progress_controller.load_progress()
+
+    assert progress_data[ProgressState.DOWNLOADING][dummy_key] == dummy_item
